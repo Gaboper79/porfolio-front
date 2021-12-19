a2c8f5ebf69c647b3d325333418cceabfe139cfe
Gaboper79 <gp130579@gmail.com>
2021-12-18 22:33:14 -0300

    primer comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = "portfolio";
+  faAngry = faEdit;
 }
