7bade7b6409b662d0809cec42f76f06d6371ae16
Gaboper79 <gp130579@gmail.com>
2022-05-09 16:43:15 -0300

    envoriomente 2

--- src/app/servicios/portfolio.service.ts
+++ src/app/servicios/portfolio.service.ts
@@ -7,32 +7,5 @@ import { PortfolioI } from "../model/portfolio";
   providedIn: "root",
 })
 export class PortfolioService {
-  portfolio!: PortfolioI;
-  private portf$!: Subject<PortfolioI>;
-
-  Url = "http://localhost:8080/api";
-  constructor(private http: HttpClient) {
-    this.portf$ = new Subject<PortfolioI>();
-  }
-
-  /*  getAllPortfolio(): Observable<any> {
-    return this.http.get(this.Url);
-  }
-  setPortfolio(portfolio: PortfolioI) {
-    this.portfolio = portfolio;
-    this.portf$.next(this.portfolio);
-  }
-
-  getportfolio$(): Observable<PortfolioI> {
-    return this.portf$.asObservable();
-  }
-  savePortfolio(portfolio: PortfolioI) {
-    this.http
-      .patch(this.Url + "/" + portfolio.id, portfolio)
-      .subscribe((res) => {});
-  }
-  agregoNuevoItemAPorfolio(portfolio: PortfolioI) {
-    this.http.post<PortfolioI>(this.Url, portfolio).subscribe((res) => {});
-  }
-  creoNuevoPorfolio() {}*/
+  constructor() {}
 }
